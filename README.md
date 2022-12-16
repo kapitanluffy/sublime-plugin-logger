72096545822214a59e7fedbe06c8244b54a56346
kapitanluffy <kapitanluffy@gmail.com>
2022-12-17 05:10:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+### Plugin Logger
+
+---
+
+📜 Displays the source plugin of your console logs
+
+<p align="center">
+<a href="https://www.sublimetext.com/">
+    <img src="https://img.shields.io/badge/Made%20For-Sublime-ff9800?logo=sublime%20text" /></a>
+<a href="https://packagecontrol.io/packages/plugin-logger">
+    <img src="https://img.shields.io/packagecontrol/dt/plugin-logger" /></a>
+<a href="https://github.com/kapitanluffy/sublime-plugin-logger/releases">
+    <img src="https://img.shields.io/github/v/tag/kapitanluffy/sublime-plugin-logger?label=release" /></a>
+<a href="https://patreon.com/kapitanluffy">
+    <img src="https://img.shields.io/endpoint.svg?url=https%3A%2F%2Fshieldsio-patreon.vercel.app%2Fapi%3Fusername%3Dkapitanluffy%26type%3Dpatrons&style=flat" alt="Support me on Patreon" /></a>
+<a href="https://ko-fi.com/kapitanluffy">
+    <img src="https://img.shields.io/badge/Ko--fi-Support-019cde?logo=ko-fi" /></a>
+</p>
+
+![preview](preview.gif)
+
+##### Installation
+
+- Install from `packagecontrol.io` or unpack the [zip](https://github.com/kapitanluffy/sublime-plugin-logger/archive/master.zip) in your packages directory
+
+##### Support
+
+You can always support me via [Patreon](https://www.patreon.com/kapitanluffy) or [Ko-fi](https://www.ko-fi.com/kapitanluffy)
+
+##### License
+
+[MIT](LICENSE)
---
+++ plugin_logger.py
@@ -0,0 +1,69 @@
+import sublime
+import sublime_api
+
+import io
+import sys
+import inspect
+import re
+
+
+class _PluginLogWriter(io.TextIOBase):
+    def __init__(self):
+        self.buf = None
+
+    def flush(self):
+        b = self.buf
+        self.buf = None
+
+        if b is None or len(b) == 0:
+            return
+
+        # Let's get the call stack without context
+        frames = inspect.stack(0)
+
+        if len(frames) < 3:
+            sublime_api.log_message(b)
+
+        # Plugin call should be right after 2 frames
+        plugin = self.get_plugin(frames[2])
+        msg = "[%s]: %s" % (plugin, b) if plugin is not False else b
+
+        sublime_api.log_message(msg)
+
+    def write(self, s):
+        if self.buf is None:
+            self.buf = s
+        else:
+            self.buf += s
+        if '\n' in s or '\r' in s:
+            self.flush()
+
+    def get_plugin(self, frame):
+        packages_path = sublime.packages_path()
+        installed_packages_path = sublime.installed_packages_path()
+
+        is_packages_path = re.search(r'^%s' % (re.escape(packages_path)), frame[1]) is not None
+        is_installed_packages_path = re.search(r'^%s' % (re.escape(installed_packages_path)), frame[1]) is not None
+
+        if is_packages_path is False and is_installed_packages_path is False:
+            return False
+
+        if is_packages_path:
+            loc = frame[1].replace(packages_path + "\\", "").split("\\")
+            return loc[0]
+
+        if is_installed_packages_path:
+            loc = frame[1].replace(installed_packages_path + "\\", "").split("\\")
+            return loc[0].split(".")[0]
+
+
+def plugin_loaded():
+    print("Plugin Logger plugin loaded")
+    sys.stdout = _PluginLogWriter()  # type: ignore
+    sys.stderr = _PluginLogWriter()  # type: ignore
+
+
+def plugin_unloaded():
+    print("Plugin Logger plugin unloaded")
+    sys.stdout = sublime._LogWriter() # type: ignore
+    sys.stderr = sublime._LogWriter() # type: ignore
